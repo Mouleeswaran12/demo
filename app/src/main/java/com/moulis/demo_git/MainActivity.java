51ee79de07b9d1a2a899062df747342d12c3ddb0
Mouleeswaran <moulee27waran@gmail.com>
2021-12-07 00:54:02 +0530

    Button update

--- app/src/main/java/com/moulis/demo_git/MainActivity.java
+++ app/src/main/java/com/moulis/demo_git/MainActivity.java
@@ -3,14 +3,23 @@ package com.moulis.demo_git;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Mouleeswaran", Toast.LENGTH_SHORT).show();
+        b=(Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "MOuleeswaran", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
